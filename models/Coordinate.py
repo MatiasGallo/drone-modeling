73a7c30b96f813a52fa0babd07a3242dc85de8e5
matias-gallo <matias.a.gallo.96@gmail.com>
2023-10-28 17:36:10 -0300

    Added distance traveled to log

--- models/Coordinate.py
+++ models/Coordinate.py
@@ -10,5 +10,8 @@ class Coordinate:
                           self.y + coordinate.y,
                           self.z + coordinate.z)
     
+    def dif (self, coordinate):
+        return abs(self.x - coordinate.x) + abs(self.y - coordinate.y) + abs(self.z - coordinate.z)
+    
     def __str__(self):
         return f"x: {self.x} / y: {self.y} / z: {self.z}"
--- models/Drone.py
+++ models/Drone.py
@@ -1,10 +1,13 @@
 class Drone:
 
+    totalDistance = 0
+
     def __init__(self, coordinate):
         self.coordinate = coordinate
         print("Drone's initial position is: " + str(self.coordinate))
 
     # Moves the Drone to destination
     def move(self, destination):
+        self.totalDistance = self.totalDistance + self.coordinate.dif(destination)
         self.coordinate = destination
-        print("Drone's new position is: " + str(self.coordinate))
+        print("Drone's new position is: " + str(self.coordinate) + f" [{self.totalDistance}]")
